d075753e2bda5bcace275742503ea0a2dab7f494
bta-6 <81649505+bta-6@users.noreply.github.com>
2021-07-15 00:58:33 +0300

    wtf

---
+++ sandbox/src/main/java/ru/qa/test/Square.java
@@ -0,0 +1,13 @@
+package ru.qa.test;
+
+public class Square {
+
+    public double l;
+    public Square(double len){
+        this.l = len;
+    }
+
+    public  double area(){
+        return this.l * this.l;
+    }
+}
---
+++ sandbox/src/main/java/ru/qa/test/aaa.java
@@ -0,0 +1,17 @@
+package ru.qa.test;
+
+public class aaa {
+    public static void main(String[] args) {
+
+        Square s = new Square(5);
+        System.out.println("surface " + s.l + " = " + s.area());
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("surf2 " + r.a + " * " + r.b +" = "+ r.area());
+
+    }
+
+
+
+
+
+}
